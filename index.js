f71b99dab8ffa73e27819960d2d347a8bf3104f5
dipon <diponyelena@gmail.com>
2020-01-08 12:42:33 -0800

    Added Unit Test

---
+++ index.js
@@ -0,0 +1,39 @@
+function sum(a,b) {
+    return a + b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function isAdult(age){
+    if(age > 20){
+        return true
+    } else if(age >= 0 && age < 21){
+        return false
+    } else{
+        return undefined;
+    }
+}
+
+function isEvenNumber(number){
+    return number % 2 === 0;
+}
+
+function isOddNumber(numb){
+    return numb % 2 === 1;
+}
+
+function isPalindrome(word){
+    let result = '';
+    for(let i = word.length - 1 ; i >= 0; i--){
+        result += word[i];
+    }
+    return result.toLowerCase() === word.toLowerCase();
+}
+
+function sortArrayUp(array){
+    return array.sort((a,b)=>  a - b);
+}
+
+module.exports = {sum,multiply,isAdult,isEvenNumber,isOddNumber,isPalindrome,sortArrayUp};
---
+++ test/index.spec.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+// const save = require('save');
+const {sum,multiply,isAdult,isEvenNumber,isOddNumber,isPalindrome,sortArrayUp} = require('../index.js');
+describe('function sum',()=>{
+
+    it('should variable a equal 10', ()=>{
+        assert.equal(sum(5,5),10);
+    });
+
+    it('should function not equals 0', function(){
+        assert.notEqual(sum(2,4),0);
+    });
+});
+describe('function multiply',()=> {
+    it('should multiply correctly ', () => {
+        assert.equal(multiply(3, 2), 6)
+    });
+});
+describe('function isAdult',()=>{
+    it('should return true if age is over 20',function(){
+        assert.equal(isAdult(31), true)
+    });
+});
+describe('function isEvenNumber',()=>{
+    it('should return true for even number',function(){
+        assert.equal(isEvenNumber(20),true)
+    });
+});
+describe('function isOddNumber',()=>{
+    it('should return true for odd number', function(){
+        assert.equal(isOddNumber( 5),true)
+    });
+});
+describe('function isPalindrome',()=>{
+    it('should return true if word is Palindrome', function(){
+        assert.equal(isPalindrome('ogo'),true)
+    });
+});
+describe('function sortArrayUp',()=>{
+    it('should return sorted array from low to high', function () {
+        assert.deepEqual(sortArrayUp([2, 3, 4, 0, 1]),[0, 1, 2, 3, 4])
+    });
+});
